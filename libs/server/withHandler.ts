ae0945eb10c705b5cf30849bb27eb5faa318288d
dongdong <crisish@naver.com>
2022-07-18 08:52:43 +0900

    #8.4 withHandler (12:58)

---
+++ libs/server/withHandler.ts
@@ -0,0 +1,22 @@
+import { NextApiRequest, NextApiResponse } from "next";
+
+export default function withHandler(
+  method: "GET" | "POST" | "DELETE",
+  fn: (req: NextApiRequest, res: NextApiResponse) => void
+) {
+  return async function (req: NextApiRequest, res: NextApiResponse) {
+    if (req.method !== method) {
+      // 요청한 method와 실행할 api method 다른 경우
+      // api url로 접속시도하는 경우 접근차단 가능
+      return res.status(405).end();
+    }
+    try {
+      // 정상요청 경우, function 실행
+      await fn(req, res);
+    } catch (error) {
+      // 에러 발생시 처리
+      console.log(error);
+      return res.status(500).json({ error }); // server error 발생시 Json으로 반환
+    }
+  };
+}
--- pages/api/users/enter.tsx
+++ pages/api/users/enter.tsx
@@ -1,17 +1,12 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import client from "../../../libs/server/client";
+import withHandler from "../../../libs/server/withHandler";
 
-export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse
-) {
-  // API 요청 타입 검증하기
-  // 정상 : POST, 비정상 : GET
-  if (req.method !== "POST") {
-    res.status(401).end();
-  }
+async function handler(req: NextApiRequest, res: NextApiResponse) {
   console.log(req.body);
   res.json({
     ok: true,
   });
 }
+
+export default withHandler("POST", handler);
