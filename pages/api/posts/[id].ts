6bce7b4b8e5d176b6879321b95e47a1a30504295
dongdong <crisish@naver.com>
2022-08-11 20:37:51 +0900

    #12.2 Post Detail (12:25)

---
+++ pages/api/posts/[id].ts
@@ -0,0 +1,56 @@
+import client from "@libs/server/client";
+import withHandler, { ResponseType } from "@libs/server/withHandler";
+import { NextApiRequest, NextApiResponse } from "next";
+import { withIronSessionApiRoute } from "iron-session/next";
+import { withIronSession } from "@libs/server/withSession";
+
+async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<ResponseType>
+) {
+  const {
+    query: { id },
+  } = req;
+
+  const post = await client.post.findUnique({
+    where: {
+      id: +id.toString(),
+    },
+    include: {
+      user: {
+        select: {
+          id: true,
+          name: true,
+          avatar: true,
+        },
+      },
+      answers: {
+        select: {
+          id: true,
+          answer: true,
+          user: {
+            select: {
+              id: true,
+              name: true,
+              avatar: true,
+            },
+          },
+        },
+      },
+      _count: {
+        select: {
+          answers: true,
+          wonderings: true,
+        },
+      },
+    },
+  });
+
+  res.json({
+    ok: true,
+    post,
+  });
+}
+
+//
+export default withIronSession(withHandler({ methods: ["GET"], handler }));
